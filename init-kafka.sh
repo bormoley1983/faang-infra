b9d75024f556288cf1145ce8e4ca66873b8edf5f
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2025-03-01 18:31:05 +0200

    partitions removed

--- init-kafka.sh
+++ init-kafka.sh
@@ -33,7 +33,7 @@ for TOPIC_CONFIG in \
   "analytics_comment_topic:1:1" \
   "user_ban_topic:1:1" \
   "userViewProfileTopic:3:1" \
-  "event_start_topic:3:1"
+  "event_start_topic:1:1"
 do
   TOPIC=$(echo $TOPIC_CONFIG | cut -d: -f1)
   PARTITIONS=$(echo $TOPIC_CONFIG | cut -d: -f2)
